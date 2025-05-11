df9a1136146f401124cab204d465149cb95f7604
ali_khanix <khaniali568@gmail.com>
2025-05-11 11:11:09 +0330

    workink on the button and card price and cta

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -9,23 +9,28 @@ export default function CartPage() {
   const total = cart.reduce((sum, item) => sum + item.price, 0);
 
   return (
-    <div>
+    <div className="px-4">
       <TopNavbar />
-      <div className="mt-[140px]">
+      <div className="mt-[140px] h-[100vh]">
         <h1 className="text-2xl font-semibold mb-4">سبد خرید شما</h1>
         {cart.length === 0 ? (
           <p>سبد شما خالی است</p>
         ) : (
-          <>
+          <div className="">
             <ul className="space-y-2">
               {cart.map((item, idx) => (
-                <li key={idx} className="border p-2 rounded">
-                  {item.name} - ${item.price.toFixed(2)}
+                <li key={idx} className="border py-2 rounded">
+                  {item.name} - {item.price.toLocaleString()}
                 </li>
               ))}
             </ul>
-            <p className="mt-4 font-bold">Total: ${total.toFixed(2)}</p>
-          </>
+            <div>
+              <p className="mt-4 font-bold flex justify-between items-center">
+                <span>جمع سبد خرید:</span>
+                <span>{total.toLocaleString()} تومان</span>
+              </p>
+            </div>
+          </div>
         )}
       </div>
 
--- src/components/ProductCart.tsx
+++ src/components/ProductCart.tsx
@@ -1,23 +1,51 @@
-import { MenuItem } from "@/types/product-types";
+import { MenuItem } from '@/types/product-types'
+import Image from 'next/image'
+import { Button } from './ui/button'
 
 interface ProductCartProps {
-  product: MenuItem;
-  onAdd: () => void;
+  product: MenuItem
+  onAdd: () => void
 }
 
-const ProductCart = ({ product, onAdd }: ProductCartProps) => (
-  <div className="border p-4 rounded shadow-sm">
-    <h2 className="text-lg font-bold">{product.name}</h2>
-    <p>توضیحات: {product.description}</p>
-    <p>قیمت: {product.price.toLocaleString()}</p>
+const ProductCart = ({ product, onAdd }: ProductCartProps) => {
+  return (
+    <div className="bg-white rounded-2xl overflow-hidden p-2 ">
+      {/* Image */}
+      <div className="relative overflow-hidden rounded-2xl w-full h-32">
+        <Image
+          fill
+          className="object-cover"
+          src={product.image}
+          alt={product.description}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </div>
 
-    <button
-      onClick={onAdd}
-      className="mt-2 bg-gray-700 text-white px-4 py-1 rounded"
-    >
-      Add to Cart
-    </button>
-  </div>
-);
+      {/* Content */}
+      <div className="p-2 rounded">
+        <div>
+          <h2 className="text-lg font-semibold">{product.name}</h2>
+          <p className="whitespace-nowrap overflow-hidden text-ellipsis text-[12px] text-gray-700">
+            {product.description}
+          </p>
+        </div>
+        <div>
+          <p className="text-gray-800 font-semibold mt-2">
+            {product.price.toLocaleString()}{' '}
+            <span className="text-[12px]">تومان</span>
+          </p>
+        </div>
 
-export default ProductCart;
+        <Button
+          variant={'default'}
+          onClick={onAdd}
+          className="mt-2 w-full px-6 py-2 rounded opa"
+        >
+          اضافه به کارت
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCart
