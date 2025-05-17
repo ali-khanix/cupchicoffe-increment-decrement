6038760ebafdcb0a14a2902028d58cc8c1f923e8
ali_khanix <khaniali568@gmail.com>
2025-05-17 11:42:28 +0330

    shadow

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -8,7 +8,7 @@ import { drinks } from '@/data/products'
 const TopNavbar = () => {
   const categories = Array.from(new Set(drinks.map((p) => p.category)))
   return (
-    <nav className="bg-gray-100 dark:bg-zinc-800 max-h-40 fixed top-0 left-0 w-full z-50 px-6 flex flex-col justify-center items-center shadow-[0px_1px_3px_3px_rgb(165,165,165)] dark:shadow-[0px_0px_15px_1px_rgba(1,1,1,.5)]">
+    <nav className="bg-gray-100 dark:bg-zinc-800 max-h-40 fixed top-0 left-0 w-full z-50 px-6 flex flex-col justify-center items-center shadow-[0px_1px_3px_3px_rgb(165,165,165)] dark:shadow-[0px_0px_15px_1px_rgba(1,1,1,.75)]">
       <Logo />
       <TagNav categories={categories} />
     </nav>
