298c9988b66178e118b010a6ca7b7727b1a6a0c6
ali_khanix <khaniali568@gmail.com>
2025-05-18 12:39:00 +0330

    animations added

--- src/app/products/components/LoadingScreen.tsx
+++ src/app/products/components/LoadingScreen.tsx
@@ -1,11 +1,15 @@
 // app/products/components/LoadingScreen.tsx
 
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+
 export default function LoadingScreen() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <span className="text-lg font-medium animate-pulse">
-        درحال بارگذاری اطلاعات...
-      </span>
+    <div className=" flex items-center justify-center min-h-screen">
+      <DotLottieReact
+        src="https://lottie.host/fb75190c-f0dd-42d0-96f3-36f51557c4c6/qUU5fiAaXu.lottie"
+        loop
+        autoplay
+      />
     </div>
   )
 }
