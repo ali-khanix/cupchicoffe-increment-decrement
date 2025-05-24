0e85527c10c1091909dc0612aabd6b88a5576ca4
ali_khanix <khaniali568@gmail.com>
2025-05-24 10:12:58 +0330

    more data added

--- src/data/products.ts
+++ src/data/products.ts
@@ -142,12 +142,213 @@ export const drinks: ProductCategory[] = [
       },
     ],
   },
+
+  {
+    category: 'نوشیدنی های گرم بر پایه اسپرسو',
+    items: [
+      {
+        id: 17,
+        name: 'اسپرسو دبل - 60 م.ل',
+        description:
+          '30/70 - بیست میلی گرم پودر قهوه اسپرسو تازه - 120 میلی گرم کافئین - 60 میلی لیتر',
+        price: 55000,
+        image:
+          'https://d1w7312wesee68.cloudfront.net/9-Eb-xSzREmwv_KV-tsNxV4aTZo3fcsFBhkdys0kzcw/resize:fit:720:720/plain/s3://toasttab/menu_service/restaurants/a1d56734-b38f-4cd5-ad68-76b845a69f3b/MenuItem/d255b9c8-e7b0-40dc-9b6b-be04ca4f96bc.jpg',
+      },
+      {
+        id: 18,
+        name: 'اسپرسو سینگل - 30 م.ل',
+        description:
+          '30/70 - نه میلی گرم پودر قهوه اسپرسو تازه - 120 میلی گرم کافئین - 60 میلی لیتر',
+        price: 50000,
+        image:
+          'https://richiamocoffee.com/wp-content/uploads/2023/04/1.-Espresso-600x600.jpg',
+      },
+      {
+        id: 19,
+        name: 'اسپرسو ماکیاتو',
+        description: 'اسپرسو، 2 قاشق فوم شیر',
+        price: 65000,
+        image: 'https://sipnnailspa.com/wp-content/uploads/2023/07/3.1.jpg',
+      },
+      {
+        id: 20,
+        name: 'اسپرسو تونیک',
+        description: 'اسپرسو، یخ، تونیک آب سرد، برش لیمو یا پوست پرتقال',
+        price: 65000,
+        image:
+          'https://res.cloudinary.com/htt8g4cd/images/f_auto,q_auto/v1730823639/wp/07_23_Espresso_Tonic_HERO_Stocksy_4062559_1920x1280/07_23_Espresso_Tonic_HERO_Stocksy_4062559_1920x1280.jpg?_i=AA',
+      },
+      {
+        id: 21,
+        name: 'آمریکانو',
+        description: 'اسپرسو، 110 م.ل آب داغ',
+        price: 65000,
+        image:
+          'https://greenbeans.ir/wp-content/uploads/2024/04/American-coffee.webp',
+      },
+      {
+        id: 22,
+        name: 'کاپوچینو',
+        description: 'اسپرسو، 60 م.ل شیر داغ، فوم شیر',
+        price: 80000,
+        image:
+          'https://tavanafamily.com/wp-content/uploads/2024/02/%D8%A8%D9%87%D8%AA%D8%B1%DB%8C%D9%86-%D9%85%D8%A7%D8%B1%DA%A9%E2%80%8C%D9%87%D8%A7%DB%8C-%DA%A9%D8%A7%D9%BE%D9%88%DA%86%DB%8C%D9%86%D9%883.jpg',
+      },
+      {
+        id: 23,
+        name: 'کاپوچینو کارامل',
+        description: 'اسپرسو، شیر داغ، فوم شیر، شربت کارامل، پودر دارچین',
+        price: 90000,
+        image:
+          'https://esfahancoffee.com/uploads/b919c064e8f14020a47d53bc1335787a.jpg',
+      },
+      {
+        id: 24,
+        name: 'کاپوچینو فندق',
+        description: 'اسپرسو، شیر داغ، فوم شیر، شربت فندق، پودر فندق',
+        price: 90000,
+        image:
+          'https://www.webstaurantstore.com/images/products/large/498984/2522423.jpg',
+      },
+      {
+        id: 25,
+        name: 'لاته',
+        description: 'اسپرسو، شیر داغ، فوم شیر',
+        price: 80000,
+        image:
+          'https://www.allrecipes.com/thmb/SUs7po94w7k2OwqYDjC3H_ZW3JQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/96629-cafe-latte-ddmfs-hero-4x3-0288359d9c37485fa69afe5369dbcf2e.jpg',
+      },
+      {
+        id: 26,
+        name: 'لاته فندق',
+        description: 'اسپرسو، شیر، شربت فندق، فوم شیر، پودر فندق',
+        price: 90000,
+        image:
+          'https://www.whiskaffair.com/wp-content/uploads/2023/01/Starbucks-Copycat-Hazelnut-Latte-Step-6.jpg',
+      },
+      {
+        id: 27,
+        name: 'لاته جینجر',
+        description: 'اسپرسو، شیر، زنجبیل، عسل، فوم شیر، پودر دارچین',
+        price: 90000,
+        image:
+          'https://thecleaneatingcouple.com/wp-content/uploads/2018/06/turmeric-latte-1.jpg',
+      },
+      {
+        id: 28,
+        name: 'لاته پسته',
+        description: 'اسپرسو، کرم پسته، شیر، عسل، وانیل، خامه',
+        price: 105000,
+        image:
+          'https://www.simplyscratch.com/wp-content/uploads/2023/02/Pistachio-Oatmilk-Latte-l-SimplyScratch-15.jpg',
+      },
+      {
+        id: 29,
+        name: 'کارامل ماکیاتو',
+        description: 'اسپرسو، شیر، وانیل، فوم شیر، سس کارامل',
+        price: 90000,
+        image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQNBTVb9Gq1RH_X8GAi5pujcvmR-HJlZCJw&s',
+      },
+      {
+        id: 30,
+        name: 'قهوه ترک',
+        description: 'قهوه ترک، شکر',
+        price: 70000,
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/T%C3%BCrk_Kahvesi_-_Bakir_Cezve.jpg/960px-T%C3%BCrk_Kahvesi_-_Bakir_Cezve.jpg',
+      },
+
+      {
+        id: 31,
+        name: 'کورتادو',
+        description: 'اسپرسو، شیر داغ',
+        price: 75000,
+        image:
+          'https://lh3.googleusercontent.com/proxy/ezfbhHF-l0THq4DM40EtdpWhjIf1XGmYiHps3Rf2Op_Td-8KmEOQqZa5aFf120Wvezd4q1_Syw-DPYhWa69KZgZHYVd3GoICNimjytjtSrkZPcZM1Ksy_3MhLkLLUcu8Q9UQB3VB4kU7E1o',
+      },
+      {
+        id: 32,
+        name: 'دمنوش مخصوص',
+        description:
+          'گل گاوزبان، بابونه، چوب دارچین، زنجبیل، هل، برگ نعنا، گل محمدی، عسل یا شیره انگور، آب جوش',
+        price: 90000,
+        image:
+          'https://www.whatsoutaddis.com/wp-content/uploads/2023/08/tea.jpg',
+      },
+    ],
+  },
+
+  {
+    category: 'نوشیدنی های سرد بر پایه اسپرسو',
+    items: [
+      {
+        id: 33,
+        name: 'آیس آمریکانو',
+        description: 'اسپرسو، آب سرد، یخ',
+        price: 75000,
+        image:
+          'https://lh3.googleusercontent.com/proxy/Slm8x7b7zOlBggLkqI2ElPniqYPeoOy1DNObj7scnPQWR4dywW6PtJ7Hi_9wVst2d6llJQZ5duMvSnwnc-VhB5SgTP5bQISLF7hzx_j1zJ8TMDxavMcMdE7KanSVqxTvpusj',
+      },
+
+      {
+        id: 34,
+        name: 'آیس لاته',
+        description: 'اسپرسو، شیر سرد، یخ، خامه',
+        price: 85000,
+        image:
+          'https://www.peanutbutterandfitness.com/wp-content/uploads/2023/08/Smores-Iced-Latte-Recipe-7.jpg',
+      },
+      {
+        id: 35,
+        name: 'آیس لاته فندق',
+        description: 'اسپرسو، شیر سرد، شربت فندق، یخ، پودر فندق',
+        price: 95000,
+        image:
+          'https://cbtl-images.s3.us-west-1.amazonaws.com/Production/Drupal/s3fs-public/styles/cafe_menu_item/public/cafe-menu/Iced_Hazelnut_Latte.jpg',
+      },
+      {
+        id: 36,
+        name: 'آیس لاته زعفران',
+        description: 'اسپرسو، شیر سرد، زعفران، عسل، خامه',
+        price: 95000,
+        image:
+          'https://lh4.googleusercontent.com/proxy/hrkoFYrnTmDr8iT55gCleCYnoCgkw_kNLplSCml_BVunPnjoF8vvyRutM7nMsIqIkAgBLriYDkk9UbgYtmywksLxW9LjLyRm5v88GZQKDe-7FhO5Kvxmrr_3m0lS',
+      },
+      {
+        id: 37,
+        name: 'آیس موکا',
+        description:
+          'اسپرسو، شیر سرد، سس شکلات یا پودر کاکائو، یخ، خامه، کاکائو رنده شده',
+        price: 95000,
+        image:
+          'https://www.moccona.com.au/siteassets/our-recipes/moccona-recipes-rich-espresso-iced-latte.jpg',
+      },
+      {
+        id: 38,
+        name: 'آیس کارامل ماکیاتو',
+        description: 'اسپرسو، شیر سرد، شربت وانیل، یخ، سس کارامل، خامه',
+        price: 95000,
+        image:
+          'https://www.sugarandsoul.co/wp-content/uploads/2021/09/iced-caramel-macchiato-10.jpg',
+      },
+      {
+        id: 39,
+        name: 'آفوگاتو',
+        description: 'بسنتی، اسپرسو',
+        price: 95000,
+        image:
+          'https://static01.nyt.com/images/2021/08/15/magazine/affogato/affogato-mediumSquareAt3X-v2.jpg',
+      },
+      {
+        id: 40,
+        name: 'آیس آمریکانو تک',
+        description: 'اسپرسو، آب سرد، یخ',
+        price: 70000,
+        image:
+          'https://lh3.googleusercontent.com/proxy/Slm8x7b7zOlBggLkqI2ElPniqYPeoOy1DNObj7scnPQWR4dywW6PtJ7Hi_9wVst2d6llJQZ5duMvSnwnc-VhB5SgTP5bQISLF7hzx_j1zJ8TMDxavMcMdE7KanSVqxTvpusj',
+      },
+    ],
+  },
 ]
-
-// const scrollToSection = (category: string) => {
-//   const section = document.getElementById(category);
-//   if(section) {
-//     section.scrollIntoView({behavior: "smooth"});
-//   }
-
-// }
